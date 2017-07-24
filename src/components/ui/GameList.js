c00c36451d6f96429590a84d520022bf370873a6
mrdokenny <mrdokenny@gmail.com>
2017-07-24 16:19:37 -0700

    Better DateTime Format in Profile

--- src/components/ui/GameList.js
+++ src/components/ui/GameList.js
@@ -1,52 +1,31 @@
 import React, { Component } from 'react';
-import { Text, ListView, StyleSheet, View } from 'react-native';
+import { Text, ScrollView, StyleSheet, View } from 'react-native';
 
 export default class GameList extends Component {
-  constructor(props) {
-    super(props);
-    let { games } = this.props;
-    // console.log(games);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
-    this.state = { dataSource: ds.cloneWithRows(games), };
-
-  }
-  componentWillReceiveProps(nextProps) {
-     if (nextProps.games !== this.props.games) {
-       let { games } = nextProps;
-       this.setState({
-         dataSource: this.state.dataSource.cloneWithRows(games)
-       });
-     }
-  }
 
   render() {
 
     return (
-    <View style={styles.container}>
+      <View style={styles.container}>
 
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={(rowData, sectionId, rowId) =>
-          <View style={styles.row}>
-            <View style={styles.column}>
-              <Text>{`Game ${parseInt(rowId) + 1}`}</Text>
-              <Text>{new Date(rowData.createdAt).toLocaleDateString()}</Text>
-            </View>
-            <Text>{`Win: ${rowData.won ? "Yes" : "No"}`}</Text>
-          </View>
+        <ScrollView>
+          {
+            this.props.games.map((item, index) => {
+              const date = new Date(item.createdAt);
+              const paddedSeconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+              return (
+                <View key={index} style={styles.row}>
+                  <View style={styles.column}>
+                    <Text>{`Game ${parseInt(index) + 1}`}</Text>
+                    <Text>{`${date.toLocaleDateString()}  ${date.getHours()}:${date.getMinutes()}:${paddedSeconds}`}</Text>
+                  </View>
+                  <Text>{`Win: ${item.won ? "Yes" : "No"}`}</Text>
+                </View>
+              );
+            })
 
-        }
-        renderSeperator={(sectionID, rowID, adjacentRowHighlighted) =>
-            <View key={rowId} style={styles.separator}/>
-        }
-        // renderSectionHeader={(sectionData, sectionId) =>
-        //   <View style={styles.rowHeader}>
-        //     <Text>Game Number</Text>
-        //     <Text>Game Won?</Text>
-        //     <Text>Date Played</Text>
-        //   </View>
-        // }
-       />
+          }
+        </ScrollView>
       </View>
     );
   }
@@ -59,20 +38,11 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     width: 350
   },
-  separator: {
-    flex: 1,
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#8E8E8E',
-  },
   row: {
     flex: 1,
     padding: 5,
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: 350
-  },
-  listView: {
-    flex: 1,
-    justifyContent: 'center',
-  },
+  }
 });
